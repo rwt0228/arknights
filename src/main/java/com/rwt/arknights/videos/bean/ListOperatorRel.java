9d513a22291a2b41025730cf70bca3adface23af
rwt0228 <610938672@qq.com>
2019-11-30 23:10:37 +0800

    修改部分数据结构，和前端命名

--- src/main/java/com/rwt/arknights/videos/bean/ListOperatorRel.java
+++ src/main/java/com/rwt/arknights/videos/bean/ListOperatorRel.java
@@ -7,8 +7,12 @@ public class ListOperatorRel {
     private Integer id;
     private Integer listId;
     private Integer operatorId;
+    private Integer jingying;
     private Integer level;
     private String skill;
     private Integer skillLevel;
+    private Integer qianneng;
+
+
 
 }
--- src/main/java/com/rwt/arknights/videos/bean/Video.java
+++ src/main/java/com/rwt/arknights/videos/bean/Video.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 @Data
 public class Video {
-    private Integer avId;
-    private String description; //描述
+    private Integer aid;
+    private String desc; //描述
     private Integer stageId;
-    private String note;    //上传备注
+    private String title;    //标题
     private String imgUrl;
     private String type;    //1作业2论文3迫害4其他类型
-
+    private String upName;
 }
